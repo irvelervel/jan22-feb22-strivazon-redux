a2bf2762de48f92dad532b9371712f80da2aedc8
Stefano Casasola <ste.casasola@gmail.com>
2022-05-11 09:57:14 +0200

    Adds the dependencies and gets the Store running

---
+++ src/app/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+export default configureStore({
+  // let's define our reducers
+  reducer: {},
+})
+
+// this file is just setting up the Redux Store, and this implementation is valid for ANY JS application
+// we want now to provide this engine to our REACT application! we have to inform our Components about it
+
+// SLICES
+// slices in Redux are store chunks. you can have as many slices as you like.
+// every slice/chunk is going to be self-mantained.
+// let's create a slice for the cart!
---
+++ src/slices/cart/cartSlice.js
@@ -0,0 +1,1 @@
+// in here we're going to define all the logic for managing the cart slice in the Redux Store
